e635595676ff57a620b23930fd4c50b1b12a780e
LealTom <leal.tom66@gmail.com>
2023-05-25 16:56:51 +0200

    ✨Add/Health bar

--- Assets/_Project/Game/Unit/Script/Component/Controller/EnemyUnitController.cs
+++ Assets/_Project/Game/Unit/Script/Component/Controller/EnemyUnitController.cs
@@ -1,8 +1,30 @@
+using MoreMountains.Tools;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class EnemyUnitController : UnitController
 {
+    private MMHealthBar healthBar;
+    private int maxHealth;
 
+    private void Awake()
+    {
+        maxHealth = HP;
+        healthBar = GetComponent<MMHealthBar>();
+        updateHealthBar(maxHealth);
+    }
+
+    public override void ReceiveDamage(int damage)
+    {
+        updateHealthBar(HP - damage);
+        base.ReceiveDamage(damage);
+    }
+
+    private void updateHealthBar(int currentHp)
+    {
+        Debug.Log($"Current HP : {currentHp}");
+        bool healthBarIsDisplay = currentHp > 0;
+        healthBar.UpdateBar(currentHp, 0, maxHealth, healthBarIsDisplay);
+    }
 }
